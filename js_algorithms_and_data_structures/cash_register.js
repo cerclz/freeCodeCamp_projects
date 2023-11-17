5d312e57bc6b06fb1ed283f2b13c542346bc1b18
Giannis Varsamis <gvar95@gmail.com>
2023-11-17 21:48:39 +0200

    CORRECT Solution for cash register for freeCodeCamp's JS Algorithms and Data Structures

--- js_algorithms_and_data_structures/cash_register.js
+++ js_algorithms_and_data_structures/cash_register.js
@@ -5,7 +5,6 @@ function checkCashRegister(price, cash, cid) {
     // Define an array to track of the change and find the change due
     let change = {}
     let changeDue = cash - price
-    console.log(changeDue)
   
     // Find total amount of money on the register
     let totalCash = 0;
@@ -13,22 +12,19 @@ function checkCashRegister(price, cash, cid) {
       totalCash += unit[1];
     }
     totalCash = totalCash.toFixed(2);
-    console.log(totalCash)
   
     // Case 1: Check if total cash in drawer is equal to change due
     // Case 2: Check if total cash in drawer is less than the change due
     if (totalCash == changeDue){
+      console.log({status: "CLOSED", change: cid})
       return {status: "CLOSED", change: cid}
     } else if (totalCash < changeDue){
-      return {status: "INSUFFICIENT_FUNDS", change: Object.entries(change)}
+      console.log("1", {status: "INSUFFICIENT_FUNDS", change: []})
+      return {status: "INSUFFICIENT_FUNDS", change: []}
     } else {
-      // Find the change due in coins and bills and save it in change array in reverse order
-  
+      // Iterate each unit in CID in reverse order
       for (let unit of cid.reverse()){
-      // Starting from the hightest unit in CID
-      // Keep getting the amount of the unit
-      // As long as change due is higher than the unit amount
-      // And there the amount is not zero in the drawer
+        // Keep removing amount of units for CID while adding them in change if the amount is sufficient 
         while (changeDue >= unitAmount[unit[0]] && unit[1] > 0){
           if (change[unit[0]]){
             change[unit[0]] += unitAmount[unit[0]]
@@ -36,19 +32,20 @@ function checkCashRegister(price, cash, cid) {
             change[unit[0]] = unitAmount[unit[0]]
           }
           changeDue -= unitAmount[unit[0]]
-          unit[1] - unitAmount[unit[0]]
+          changeDue = changeDue.toFixed(2)
+          unit[1] -= unitAmount[unit[0]]
         }
       }
-      console.log(Object.entries(change), changeDue.toFixed(2))
-  
-      // Case 3: You cannot return the exact change
-      if (changeDue != 0){
-        return {status: "INSUFFICIENT_FUNDS", change: []}
-      } else {
-        console.log({status: "OPEN", change: Object.entries(change)})
-        return {status: "OPEN", change: Object.entries(change)}
-      }
     }
+    // Check 3: Cannot return the exact change
+    if (changeDue > 0) {
+      return {status: "INSUFFICIENT_FUNDS", change: []}
+    }
+    // Return the change due in coins and bills
+    return {status: "OPEN", change: Object.entries(change)}
+  
   }
   
-  checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
+  checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
+  
+  
--- js_algorithms_and_data_structures/telephone_number_validator.js
+++ js_algorithms_and_data_structures/telephone_number_validator.js
@@ -1,50 +1,12 @@
-function checkCashRegister(price, cash, cid) {
-  // Define the value of each unit
-  const unitAmount = {"PENNY": 0.01, "NICKEL": 0.05, "DIME": 0.1, "QUARTER": 0.25, "ONE": 1, "FIVE": 5, "TEN": 10, "TWENTY": 20, "ONE HUNDRED": 100}
-
-  // Define an array to track of the change and find the change due
-  let change = {}
-  let changeDue = cash - price
-
-  // Find total amount of money on the register
-  let totalCash = 0;
-  for (let unit of cid) {
-    totalCash += unit[1];
-  }
-  totalCash = totalCash.toFixed(2);
-
-  // Case 1: Check if total cash in drawer is equal to change due
-  // Case 2: Check if total cash in drawer is less than the change due
-  if (totalCash == changeDue){
-    console.log({status: "CLOSED", change: cid})
-    return {status: "CLOSED", change: cid}
-  } else if (totalCash < changeDue){
-    console.log("1", {status: "INSUFFICIENT_FUNDS", change: []})
-    return {status: "INSUFFICIENT_FUNDS", change: []}
-  } else {
-    // Iterate each unit in CID in reverse order
-    for (let unit of cid.reverse()){
-      // Keep removing amount of units for CID while adding them in change if the amount is sufficient 
-      while (changeDue >= unitAmount[unit[0]] && unit[1] > 0){
-        if (change[unit[0]]){
-          change[unit[0]] += unitAmount[unit[0]]
-        } else{
-          change[unit[0]] = unitAmount[unit[0]]
-        }
-        changeDue -= unitAmount[unit[0]]
-        changeDue = changeDue.toFixed(2)
-        unit[1] -= unitAmount[unit[0]]
-      }
+function telephoneCheck(str) {
+    // Create a regex to match the possible patterns
+    let regex = /^(1|1\s)?([0-9]{3}(\s|-)*[0-9]{3}(\s|-)[0-9]{4}|(1|1\s|$)?[0-9]{10}$|(1|1\s|$)?[(][0-9]{3}[)](\s|-)*[0-9]{3}(\s|-)[0-9]{4})/
+    // Check if the number matches the regex we created
+    if (regex.test(str)){
+      return true
     }
+    return false
   }
-  // Check 3: Cannot return the exact change
-  if (changeDue > 0) {
-    return {status: "INSUFFICIENT_FUNDS", change: []}
-  }
-  // Return the change due in coins and bills
-  return {status: "OPEN", change: Object.entries(change)}
-
-}
-
-checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
-
+  
+  telephoneCheck(" 555)555-5555");
+  
