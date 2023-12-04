fd23b15c345646bc78ebc75492cf032bc9946407
Giannis Varsamis <gvar95@gmail.com>
2023-12-04 21:29:40 +0200

    Added boilerplate template code and bootstrap

--- front_end_dev_libraries/random_quote_machine/src/App.js
+++ front_end_dev_libraries/random_quote_machine/src/App.js
@@ -3,21 +3,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-dark text-white text-center p-3 w-25" id="quote-box">
+
+      <div id="text">
+        Quote Text!
+      </div>
+
+      <div id="author">
+        Author!
+      </div>
+
+      <div id="Game">
+        Game!
+      </div>
+
+      <button id="new-quote" className="btn btn-primary">Get Quote!</button>
+
+      <a href="#" id="tweet-quote">Tweet Quote!</a>
+
     </div>
   );
 }
